c3684288e673fd4ef28f72cfbceccda7c7c6b051
John Francis Engada <jfengada49137@liceo.edu.ph>
2026-08-13 09:10:40 +0800

    Lab Activity 6 - Inheritance

---
+++ src/Truck.java
@@ -0,0 +1,15 @@
+public class Truck extends Vehicle {
+
+    private double loadCapacity;
+
+    public Truck(String brand, int year, double loadCapacity) {
+        super(brand, year);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Truck: " + brand + " (" + year + ") - "
+                + loadCapacity + " tons");
+    }
+}
